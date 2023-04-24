b3fc3b4b656f54edff51c2175fda3b4a229a21b0
houjuunue <i@nue.im>
2023-04-24 14:54:55 +0800

    disable sementic tokens highlight

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,8 +1,38 @@
 return {
+    { "catppuccin/nvim", name = "catppuccin", enabled = false },
     {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        enabled = false,
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = function()
+                local style = "dark";
+                local colors = require "decay.core".get_colors(style)
+                local fg = colors.foreground
+                local bg = colors.background
+                local accent = colors.accent
+                local none = "NONE"
+
+                require "decay".setup {
+                    style = style,
+                    palette_overrides = {
+                        background = none,
+                    },
+                    override = {
+                        Normal = {
+                            fg = fg,
+                            bg = none,
+                        },
+                        TelescopeSelection = {
+                            fg = bg,
+                            bg = accent,
+                        },
+                    },
+                }
+
+                for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
+                  vim.api.nvim_set_hl(0, group, {})
+                end
+            end,
+        },
     },
     {
         "folke/tokyonight.nvim",
@@ -13,5 +43,10 @@ return {
                 floats = "transparent",
             },
         },
+    },
+    {
+        "decaycs/decay.nvim",
+        config = function()
+        end,
     },
 }
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,7 +4,31 @@ return {
         priority = 1000,
         import = "lazyvim.plugins",
         opts = {
-            colorscheme = "tokyonight",
+            colorscheme = function()
+                local style = "dark";
+                local colors = require "decay.core".get_colors(style)
+                local fg = colors.foreground
+                local bg = colors.background
+                local accent = colors.accent
+                local none = "NONE"
+
+                require "decay".setup {
+                    style = style,
+                    palette_overrides = {
+                        background = none,
+                    },
+                    override = {
+                        Normal = {
+                            fg = fg,
+                            bg = none,
+                        },
+                        TelescopeSelection = {
+                            fg = bg,
+                            bg = accent,
+                        },
+                    },
+                }
+            end,
         },
     },
 }
