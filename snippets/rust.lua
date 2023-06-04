4cfcd479417f73474f1ce9a7006feea9c3c8ec1e
houjuunue <i@nue.im>
2023-06-04 13:07:27 +0800

    edit rust snippets

--- snippets/rust.lua
+++ snippets/rust.lua
@@ -1,110 +1,17 @@
--- local postfix = require('luasnip.extras.postfix').postfix
+-- local postfix = require("luasnip.extras.postfix").postfix
+
+local function cpio()
+    local file = io.open(vim.fn.stdpath("config") .. "/snippets/rust/cpio.rs", "r")
+    local content = file and file:read "*all" or "// 404"
+    if file then file:close() end
+    content = string.gsub(content, "%[", "[[")
+    content = string.gsub(content, "]", "]]")
+    return content
+end
 
 return {
-    s("letm", { t({ "let mut " }) }),
-
     -- template
-    s("cpio", fmt([=[
-        type Unit = Result<(), Box<dyn std::error::Error>>;
-
-        fn solve(io: &mut io::IO) -> Unit {
-            io.print(1)?;
-            Ok(())
-        }
-
-        fn main() -> Unit {
-            let mut io = io::IO::new();
-            for _ in 0..io.read::<usize>()? {
-                solve(&mut io)?;
-            }
-            Ok(())
-        }
-
-        #[[allow(dead_code)]]
-        mod io {
-            use std::{
-                error::Error,
-                fmt::Display,
-                io::{BufRead, BufWriter, StdinLock, StdoutLock, Write, stdin, stdout},
-                str::FromStr,
-            };
-
-            macro_rules! read_impl {
-                ( $f:ident, $($t:ident),+ ) => {
-                    #[[allow(unused_parens)]]
-                    pub fn $f<$($t),+>(&mut self) -> Result<($($t),+), Box<dyn std::error::Error>>
-                    where
-                        $($t: std::str::FromStr,
-                        <$t>::Err: std::error::Error + 'static,)+
-                    {
-                        let mut s: String = String::new();
-                        self.scan.read_line(&mut s)?;
-                        let mut s = s.split_whitespace();
-                        Ok(($(s.next().ok_or("")?.parse::<$t>()?),+))
-                    }
-                };
-            }
-
-            pub struct IO<'a> {
-                scan: StdinLock<'a>,
-                out: BufWriter<StdoutLock<'a>>,
-            }
-
-            impl<'a> IO<'a> {
-                pub fn new() -> Self {
-                    Self {
-                        scan: stdin().lock(),
-                        out: BufWriter::new(stdout().lock()),
-                    }
-                }
-
-                read_impl!(read, T1);
-                read_impl!(read2, T1, T2);
-                read_impl!(read3, T1, T2, T3);
-
-                pub fn read_line(&mut self) -> Result<String, Box<dyn Error>> {
-                    let mut s: String = String::new();
-                    self.scan.read_line(&mut s)?;
-                    Ok(s)
-                }
-
-                pub fn read_vec<T, V>(&mut self, n: usize) -> Result<V, Box<dyn Error>>
-                where
-                    T: FromStr,
-                    <T>::Err: Error + 'static,
-                    V: FromIterator<T>,
-                {
-                    let mut s: String = String::new();
-                    self.scan.read_line(&mut s)?;
-                    let v = s
-                        .split_whitespace()
-                        .take(n)
-                        .map(&str::parse)
-                        .collect::<Result<V, _>>()?;
-                    Ok(v)
-                }
-
-                pub fn print<T: Display>(&mut self, value: T) -> Result<(), Box<dyn Error>> {
-                    writeln!(self.out, "{}", value)?;
-                    Ok(())
-                }
-
-                pub fn print_vec<T: Display>(
-                    &mut self,
-                    mut values: impl Iterator<Item = T>,
-                ) -> Result<(), Box<dyn Error>> {
-                    if let Some(v) = values.next() {
-                        write!(self.out, "{}", v)?;
-                        for v in values {
-                            write!(self.out, " {}", v)?;
-                        }
-                    }
-                    writeln!(self.out)?;
-                    Ok(())
-                }
-            }
-        }
-    ]=], {}, { delimiters = '[]' })),
+    s("cpio", fmt(cpio(), {}, { delimiters = "[]" })),
 }, {
     -- autosnippets
 }
