178b1c4c499c4181b197c7e911feda98c5dceb30
houjuunue <i@nue.im>
2023-07-24 22:38:48 +0800

    remove rust snippets

--- snippets/rust.lua
+++ snippets/rust.lua
@@ -1,17 +1,7 @@
 -- local postfix = require("luasnip.extras.postfix").postfix
 
-local function cpio()
-    local file = io.open(vim.fn.stdpath("config") .. "/snippets/rust/cpio.rs", "r")
-    local content = file and file:read "*all" or "// 404"
-    if file then file:close() end
-    content = string.gsub(content, "%[", "[[")
-    content = string.gsub(content, "]", "]]")
-    return content
-end
-
 return {
     -- template
-    s("cpio", fmt(cpio(), {}, { delimiters = "[]" })),
 }, {
     -- autosnippets
 }
