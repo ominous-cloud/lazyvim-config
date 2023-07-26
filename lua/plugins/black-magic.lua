b16115ca624d0a0e46d9ad85b7f35379d9af45fa
houjuunue <i@nue.im>
2023-07-27 02:39:26 +0800

    unset conceallevel for tex

--- lua/plugins/black-magic.lua
+++ lua/plugins/black-magic.lua
@@ -175,4 +175,21 @@ return {
         config = function()
         end,
     },
+    {
+        "lervag/vimtex",
+        lazy = false,     -- lazy-loading will disable inverse search
+        config = function()
+            vim.api.nvim_create_autocmd({ "FileType" }, {
+                group = vim.api.nvim_create_augroup("lazyvim_vimtex_conceal", { clear = true }),
+                pattern = { "bib", "tex" },
+                callback = function()
+                    vim.wo.conceallevel = 0
+                end,
+            })
+
+            vim.g.vimtex_mappings_disable = { ["n"] = { "K" } }
+            vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
+        end,
+    },
+
 }
