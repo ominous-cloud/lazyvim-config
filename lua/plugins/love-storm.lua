fbfe820c76b18e876c6ed89b823f70c22c2fd214
houjuunue <i@nue.im>
2023-12-11 11:18:40 +0800

    change color to default

--- lua/plugins/love-storm.lua
+++ lua/plugins/love-storm.lua
@@ -1,168 +1,30 @@
-local function colorful()
-    -- debug using vim.show_pos()
-    local style = "dark"
-    local colors = require "decay.core".get_colors(style)
-    local fg = colors.foreground
-    local bg = colors.background
-    local dark = colors.black
-    local none = "none"
-
-    local palette_overrides = {
-        dark0_hard = dark,
-        dark0 = dark,
-        dark0_soft = dark,
-        dark1 = dark,
-        dark2 = dark,
-        dark3 = dark,
-        dark4 = dark,
-        light0_hard = fg,
-        light0 = fg,
-        light0_soft = fg,
-        light1 = fg,
-        light2 = fg,
-        light3 = fg,
-        light4 = fg,
-        bright_red = fg,
-        bright_green = fg,
-        bright_yellow = fg,
-        bright_blue = fg,
-        bright_purple = fg,
-        bright_aqua = fg,
-        bright_orange = fg,
-        neutral_red = fg,
-        neutral_green = fg,
-        neutral_yellow = fg,
-        neutral_blue = fg,
-        neutral_purple = fg,
-        neutral_aqua = fg,
-        neutral_orange = fg,
-        faded_red = fg,
-        faded_green = fg,
-        faded_yellow = fg,
-        faded_blue = fg,
-        faded_purple = fg,
-        faded_aqua = fg,
-        faded_orange = fg,
-        gray = fg,
-    }
-
-    local highlight_overrides = {
-        -- LspCodeLens = {
-        --     italic = false,
-        -- },
-        Todo = {
-            fg = fg,
-            bg = none,
-        },
-        NoiceLspProgressTitle = {
-            fg = fg,
-        },
-        NeoTreeGitConflict = {
-            fg = fg,
-        },
-        NeoTreeGitUntracked = {
-            fg = fg,
-        },
-        NeoTreeModified = {
-            fg = fg,
-        },
-        NvimWindoSwitch = {
-            bg = fg,
-            fg = dark,
-            bold = false,
-            italic = false,
-        },
-        NvimWindoSwitchNC = {
-            bg = fg,
-            fg = dark,
-            bold = false,
-            italic = false,
-        },
-        StatusLine = {
-            bg = fg,
-            fg = bg,
-            italic = false,
-        },
-        StatusLineNC = {
-            bg = fg,
-            fg = bg,
-            italic = true,
-        },
-    }
-
-    require "gruvbox".setup({
-        undercurl = true,
-        underline = true,
-        bold = false,
-        italic = {
-            strings = false,
-            comments = false,
-            operators = false,
-            folds = false,
-        },
-        strikethrough = true,
-        invert_selection = false,
-        invert_signs = false,
-        invert_tabline = false,
-        invert_intend_guides = false,
-        inverse = false,
-        contrast = "",
-        dim_inactive = false,
-        transparent_mode = true,
-        palette_overrides = palette_overrides,
-        overrides = highlight_overrides,
-    })
-    vim.cmd.colorscheme "gruvbox"
-
-    for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
-        vim.api.nvim_set_hl(0, group, {})
+local function default_color()
+    local function transparent(group)
+        local none = "NONE"
+        vim.api.nvim_set_hl(0, group, { bg = none })
     end
-
-    require "nvim-web-devicons".setup {
-        color_icons = false,
-    }
-end
-
-local function decay()
-    local style = "dark";
-    local colors = require "decay.core".get_colors(style)
-    local fg = colors.foreground
-    local bg = colors.background
-    local accent = colors.accent
-    local none = "NONE"
-
-    require "decay".setup {
-        style = style,
-        palette_overrides = {
-            background = none,
-        },
-        override = {
-            Normal = {
-                fg = fg,
-                bg = none,
-            },
-            TelescopeSelection = {
-                fg = bg,
-                bg = accent,
-            },
-        },
-    }
-
-    for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
-        vim.api.nvim_set_hl(0, group, {})
+    local function link(group)
+        return function(base)
+            vim.api.nvim_set_hl(0, group, { link = base })
+        end
     end
+    vim.cmd.colorscheme "default"
+    transparent "Normal"
+    transparent "StatusLine"
+    link "Keyword" "Constant"
 end
 
 return {
     {
         "LazyVim/LazyVim",
         opts = {
-            colorscheme = "catppuccin",
+            colorscheme = default_color,
         },
     },
     {
         "catppuccin/nvim",
         name = "catppuccin",
+        enabled = false,
         opts = {
             flavour = "mocha",
             transparent_background = true,
@@ -186,6 +48,7 @@ return {
     },
     {
         "folke/tokyonight.nvim",
+        enabled = false,
         opts = {
             transparent = true,
             styles = {
@@ -194,14 +57,4 @@ return {
             },
         },
     },
-    {
-        "decaycs/decay.nvim",
-        config = function()
-        end,
-    },
-    {
-        "ellisonleao/gruvbox.nvim",
-        config = function()
-        end,
-    }
 }
