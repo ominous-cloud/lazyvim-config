d9d7c1ef8459496486226acabf02b6fdcdb99f69
houjuunue <i@nue.im>
2023-12-19 02:17:32 +0800

    update deps

--- init.lua
+++ init.lua
@@ -1,12 +1,12 @@
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
   local cmd = vim.fn.split([[
     git clone
       --filter blob:none
       https://github.com/folke/lazy.nvim.git
       --branch stable
   ]] .. lazypath)
-  vim.system(cmd)
+  vim.system(cmd, {}, function() vim.schedule(vim.cmd.qa) end)
 end
 vim.opt.rtp:prepend(lazypath)
 
