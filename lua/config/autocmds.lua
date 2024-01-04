7cea1ae505ea642bf329d98c43bae8d9dfc68468
houjuunue <i@nue.im>
2024-01-04 12:11:09 +0800

    add custom math zone

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -8,3 +8,19 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.bo.cms = "// %s"
   end,
 })
+
+vim.api.nvim_create_augroup("CustomTexNewMathZone", { clear = true })
+vim.api.nvim_create_autocmd("BufReadPost", {
+  pattern = { "*.tex" },
+  group = "CustomTexNewMathZone",
+  callback = function()
+    vim.iter({
+      "align",
+      "flalign",
+      "multline",
+      "gather",
+    }):each(function(name)
+      vim.fn.TexNewMathZone("E", name, 1)
+    end)
+  end,
+})
