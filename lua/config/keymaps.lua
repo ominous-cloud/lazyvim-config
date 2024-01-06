174c64bbe895b9c2bfc60abdb8494b4c02e0958a
houjuunue <i@nue.im>
2024-01-06 15:45:04 +0800

    fixup! add trans command

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,4 +5,4 @@ vim.keymap.set({ "n", "v" }, "H", "^")
 vim.keymap.set({ "n", "v" }, "L", "$")
 vim.keymap.set({ "n", "v" }, "M", "%")
 
-vim.api.nvim_create_user_command("Trans", "'<'>!proxychains4 -q trans :zh -b", {})
+vim.api.nvim_create_user_command("Trans", "'<'>!proxychains4 -q trans :zh -b", { range = true })
